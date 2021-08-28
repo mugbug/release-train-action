f1b6fd291cce2f5a360fed7b964c0f6b6156cde3
Pedro Zaroni <zaronipedro@outlook.com>
2021-08-28 12:44:29 -0300

    fix: env usage

--- tag.sh
+++ tag.sh
@@ -3,7 +3,7 @@
 echo "🔨 Setup environment"
 npm install
 git fetch --prune --unshallow
-git fetch --tags origin $CURRENT_BRANCH
+git fetch --tags origin $STABLE_BRANCH
 git config --global user.name "github-actions"
 git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
 
@@ -15,7 +15,7 @@ git push origin $PACKAGE_VERSION
 echo "🚀 Create release"
 PACKAGE_VERSION=v$(grep version package.json | cut -c 15- | rev | cut -c 3- | rev)
 gh release create \
---target $CURRENT_BRANCH \
+--target $STABLE_BRANCH \
 --title $PACKAGE_VERSION \
 $PACKAGE_VERSION \
 --notes-file RELEASE_NOTES.md
