7ff423effa22193b641aeb5d3f3adfd6dba81ccc
Pedro Zaroni <zaronipedro@outlook.com>
2021-08-28 12:50:14 -0300

    fix: action script

--- action.sh
+++ action.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ $COMMAND = 'release-train']; then
+if [ "$COMMAND" = "release-train" ]; then
     $ROOT_PATH/release-train.sh
 fi
 
-if [ $COMMAND = 'tag']; then
+if [ "$COMMAND" = "tag" ]; then
     $ROOT_PATH/tag.sh
 fi
