323c4e4efa14dda816a11b328ec8ba89a5e38100
Pedro Zaroni <zaronipedro@outlook.com>
2021-09-01 08:04:06 -0300

    wip

--- cmds.sh
+++ cmds.sh
@@ -121,14 +121,7 @@ function open_pull_request_to_development_branch() {
   gh pr create --fill \
       --reviewer $RELEASE_OWNER \
       --assignee $RELEASE_OWNER \
-      --label release-train \
-      --base $STABLE_BRANCH \
-      --title "release: $PACKAGE_VERSION" \
-      --body "$(cat RELEASE_NOTES.md)"
-  gh pr create --fill \
-      --reviewer $RELEASE_OWNER \
-      --assignee $RELEASE_OWNER \
-      --label release-train \
+      # --label release-train \
       --base $DEVELOPMENT_BRANCH \
       --title "release: $PACKAGE_VERSION (update $DEVELOPMENT_BRANCH)" \
       --body "$(cat RELEASE_NOTES.md)"
