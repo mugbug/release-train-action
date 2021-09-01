7cbbab07f41468455ed33302b353fe3d4799ded3
Pedro Zaroni <zaronipedro@outlook.com>
2021-08-31 21:27:43 -0300

    wip

--- pr-linter.sh
+++ pr-linter.sh
@@ -3,9 +3,9 @@
 source $ROOT_PATH/cmds.sh
 
 login_to_github_cli
-lint_pr_title "TITLE_LINT_MESSAGE.md"
+lint_pr_title $TITLE_LINT_MESSAGE
 EXIT_CODE=$?
-lint_pr_destination_branch "BASE_BRANCH_LINT_MESSAGE.md"
+lint_pr_destination_branch $BASE_BRANCH_LINT_MESSAGE
 
 if [[ $? != 0 || $EXIT_CODE != 0 ]]; then 
     exit $?
